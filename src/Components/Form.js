bd6984b06d66f3088194db1038071a9c6185028a
NitaLewska <Nita@ya.ru>
2022-06-22 15:51:00 +0300

    feat: random img

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,31 @@
 import "./Form.css"
+import memesData from "../memesData"
+import React from "react"
 
 export default function Form() {
+
+        function getMemeImage() {
+            const memesArray = memesData.data.memes
+            const randomNumber = Math.floor(Math.random() * memesArray.length)
+            setMemeImage(memesArray[randomNumber].url)
+        }
+
+        const [memeImage, setMemeImage] = React.useState('')
+
     return(
         <form>
-            <input type="text"></input>
-            <input type="text"></input>
-            <button>Get a new meme image  🖼</button>
+            <input 
+            type="text"
+            placeholder="Top text"
+            ></input>
+            <input 
+            type="text"
+            placeholder="Bottom text"
+            ></input>
+            <button
+            type="button" onClick={getMemeImage}>Get a new meme image  🖼</button>
+            <img src={memeImage} alt=""></img>
         </form>
+        
     )
 }
